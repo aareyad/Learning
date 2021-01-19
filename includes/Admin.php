8ee28e19c331da40d086ed20723c71ee12e99390
Ali Akbar Reyad <aliakbar.reyad@gmail.com>
2021-01-20 00:16:06 +0600

    Added form

--- includes/Admin.php
+++ includes/Admin.php
@@ -13,6 +13,14 @@ class Admin {
      * Initializes the class
      */
     public function __construct() {
+        $this->dispatch_actions();
+
         new Admin\Menu();
+    }
+
+    public function dispatch_actions() {
+        $addressbook = new Admin\Addressbook();
+
+        add_action( 'admin_init', [ $addressbook, 'form_handler' ] );
     }
 }
